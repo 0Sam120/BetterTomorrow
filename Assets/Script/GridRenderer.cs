b04b7bb390d80d64539c48d6b99eb057ac01672b
ninjabsbx <130876365+ninjabsbx@users.noreply.github.com>
2025-04-28 00:08:31 -0300

    Update GridRenderer.cs

--- Assets/Script/GridRenderer.cs
+++ Assets/Script/GridRenderer.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 
 public class GridRenderer : MonoBehaviour
 {
-    GridMap grid;
-    [SerializeField] GameObject movePoint;
-    List<GameObject> movePointGo;
+    GridMap grid; // Reference to the GridMap component
+    [SerializeField] GameObject movePoint; // Prefab for move point visualization
+    List<GameObject> movePointGo; // List to hold spawned move point GameObjects
 
     private void Start()
     {
-        grid = GetComponent<GridMap>();
-        movePointGo = new List<GameObject>();
+        grid = GetComponent<GridMap>(); // Get the GridMap component attached to the same GameObject
+        movePointGo = new List<GameObject>(); // Initialize the list of move points
     }
 }
