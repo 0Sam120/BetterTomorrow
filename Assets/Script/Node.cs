b004cb61bbe631bd8cd684268e6b714487826394
ninjabsbx <130876365+ninjabsbx@users.noreply.github.com>
2025-04-29 00:04:59 -0300

    Update Node.cs

--- Assets/Script/Node.cs
+++ Assets/Script/Node.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Represents a single cell/node in the grid
 public class Node
 {
-    public bool passable;
-    public GridObject gridObject;
-    public float elevation;
+    public bool passable; // Whether this node can be walked on or not
+    public GridObject gridObject; // Reference to any object placed on this node
+    public float elevation; // Height (Y-axis value) of the terrain at this node
 }
