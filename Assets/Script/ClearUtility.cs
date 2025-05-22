0998a0f4522b388eed5c015907f055745e918a64
ninjabsbx <130876365+ninjabsbx@users.noreply.github.com>
2025-05-21 23:20:34 -0300

    Update ClearUtility.cs
    Added comments

--- Assets/Script/ClearUtility.cs
+++ Assets/Script/ClearUtility.cs
@@ -2,25 +2,34 @@ using UnityEngine;
 
 public class ClearUtility : MonoBehaviour
 {
+    // Reference to the pathfinding system
     [SerializeField] Pathfinding targetPF;
+
+    // Reference to the grid renderer used for attack highlights
     [SerializeField] GridRenderer attackHighlight;
+
+    // Reference to the grid renderer used for movement highlights
     [SerializeField] GridRenderer moveHighlight;
 
+    // Clears the pathfinding nodes (used to reset movement paths)
     public void ClearPathfinding()
     {
         targetPF.ClearNodes();
     }
 
+    // Hides the grid highlight for attack range
     public void ClearGridHighlightAttack()
     {
         attackHighlight.Hide();
     }
 
+    // Hides the grid highlight for movement range
     public void ClearGridHighlightMove()
     {
         moveHighlight.Hide();
     }
 
+    // Fully clears all pathfinding and grid highlights (attack and move)
     public void FullClear()
     {
         ClearPathfinding();
